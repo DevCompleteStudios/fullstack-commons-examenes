d9bdc70172645cdc378082d82932d065dc559e1e
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-04 20:14:18 -0600

    Agregando entidades y lista para usar como dependencia

--- src/main/java/com/devstudios/microservicios/commons/examenes/CommonsExamenesApplication.java
+++ src/main/java/com/devstudios/microservicios/commons/examenes/CommonsExamenesApplication.java
@@ -1,15 +1,8 @@
 package com.devstudios.microservicios.commons.examenes;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
-public class CommonsExamenesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommonsExamenesApplication.class, args);
-	}
-
-}
+public class CommonsExamenesApplication {}
