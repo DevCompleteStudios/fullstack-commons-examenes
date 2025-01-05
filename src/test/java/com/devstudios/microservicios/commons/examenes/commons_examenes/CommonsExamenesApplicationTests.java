78a61c8c872c71cbad63f55fdc2115ad431e28ff
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-04 20:07:53 -0600

    First commit

---
+++ src/test/java/com/devstudios/microservicios/commons/examenes/commons_examenes/CommonsExamenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.microservicios.commons.examenes.commons_examenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsExamenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
